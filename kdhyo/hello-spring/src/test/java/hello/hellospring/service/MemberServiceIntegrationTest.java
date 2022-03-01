01e1f624cb22092c79e0177d7fed5bc8a47428fa
dongHyo <kdhyo98@gmail.com>
2022-03-01 15:01:14 +0900

    test : spring boot Integration test

---
+++ kdhyo/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,54 @@
+package hello.hellospring.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
+
+	@Autowired
+	MemberService memberService;
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Test
+	void 회원가입() {
+		// given
+		Member member = new Member();
+		member.setName("spring");
+
+		// when
+		Long saveId = memberService.join(member);
+
+		// then
+		Member findMember = memberService.findOne(saveId).get();
+		assertThat(member.getName()).isEqualTo(findMember.getName());
+	}
+
+	@Test
+	void 중복_회원_예외() {
+		// given
+		Member member1 = new Member();
+		member1.setName("spring");
+
+		Member member2 = new Member();
+		member2.setName("spring");
+
+		// when
+		memberService.join(member1);
+
+		// then
+		assertThatExceptionOfType(IllegalStateException.class)
+			.isThrownBy(() -> memberService.join(member2))
+			.withMessage("이미 존재하는 회원 입니다.");
+	}
+
+}
